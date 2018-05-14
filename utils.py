24b81fe72cb70d16bfc073f4e2d550943e0ea2d4
Michael Liu <miliu@protonmail.com>
2018-05-13 20:50:28 -0700

    escape quotation marks just in case

--- utils.py
+++ utils.py
@@ -2,5 +2,5 @@ def clean_word(word: str) -> str:
     return word.strip()                   \
                .lower()                   \
                .replace('’', '\'')        \
-               .lstrip('<(\'"“')          \
-               .rstrip('>)\'"”.,!?:;—-…')
+               .lstrip('<(\'\"“')          \
+               .rstrip('>)\'\"”.,!?:;—-…')
