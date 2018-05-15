7efc176a63d061b21d1c42e9259b59c2b5a73638
Michael Liu <miliu@protonmail.com>
2018-05-15 12:42:29 -0700

    clear testing code because everything totally works perfectly

--- train.py
+++ train.py
@@ -1,20 +1,3 @@
 import torch
 
-import utils
-
-
-corpus = "This is the entire corpus. Seriously, it's contained in here. All of it."
-print("Corpus: \n\t{}".format(corpus))
-
-vocabulary = utils.Vocabulary(corpus=corpus)
-print("`vocabulary` initialized successfully.")
-print("Size of `vocabulary`: {}".format(vocabulary.size))
-print("Contents of `vocabulary`:")
-for word in vocabulary.words:
-    print("\t{}".format(word))
-
-indices = vocabulary.indexify(["all", "this"])
-print("[\"all\", \"this\"] indexified: \n\t{}".format(indices))
-
-words = vocabulary.wordify(indices)
-print("and converted back into words: \n\t{}".format(words))
+import model
