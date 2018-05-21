c5d08de0b24564826e10dc6f596a5fde44944081
Michael Liu <miliu@protonmail.com>
2018-05-21 16:32:58 -0700

    implement Encoder

--- model.py
+++ model.py
@@ -4,11 +4,24 @@ from torch.nn import functional as func
 
 
 class Encoder(nn.Module):
-    def __init__(self):
+    def __init__(self, vocab_size, hidden_size):
         super(Encoder, self).__init__()
-        raise NotImplementedError()
+
+        self.hidden = torch.zeros(2, 1, hidden_size)
+
+        self.embedding = nn.Embedding(vocab_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True, bidirectional=True)
+
+    def forward(self, input_seq):
+        output              = self.embedding(input_seq)
+        output, self.hidden = self.gru(output, self.hidden)
+
+        return output
 
 class Decoder(nn.Module):
     def __init__(self):
         super(Decoder, self).__init__()
+        raise NotImplementedError()
+
+    def forward(self):
         raise NotImplementedError()
