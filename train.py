5598b39a2cff9c87eb2a789b9340130e18c2a9e0
Michael Liu <miliu@protonmail.com>
2018-09-04 15:47:53 -0700

    extend test

--- train.py
+++ train.py
@@ -1,3 +1,12 @@
 import torch
 
-import model
+from utils import *
+from model import *
+
+
+vocab = Vocabulary(corpus="This is the entire vocabulary. Yep, everything. All the words. Yeah, that is it.")
+input_seq = vocab.sequence("This is it.".split())
+print("Input Sequence: {}".format(input_seq))
+
+encoder = Encoder(vocab_size=vocab.size, hidden_size=128)
+output = encoder.forward(torch.tensor(input_seq).unsqueeze(0))
